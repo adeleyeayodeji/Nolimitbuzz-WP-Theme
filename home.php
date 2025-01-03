866e754db05a186bfbbec4697acd3eba8e6cbf8a
adeleyeayodeji <biodunhi@gmail.com>
2025-01-03 18:03:08 +0100

    Add new section to home page and update styles
    - Introduced a new section (no-limit-buzz--site-main-section-2) in home.php with content and layout.
    - Updated CSS styles for the new section, including layout adjustments and responsive design.
    - Increased max-width for various elements in custom CSS and style files for better alignment.
    - Enhanced styling for text elements within the new section for improved readability.

--- home.php
+++ home.php
@@ -33,6 +33,30 @@ get_header();
         </div>
     </section>
 
+    <section class="no-limit-buzz--site-main-section-2">
+        <div class="no-limit-buzz--site-main-section-2-container">
+            <div class="no-limit-buzz--site-main-section-2-left">
+                <div class="no-limit-buzz--site-main-section-2-left-title">
+                    <h3>long established</h3>
+                    <p>
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that....
+                    </p>
+                </div>
+                <div class="no-limit-buzz--site-main-section-2-left-date">
+                    <span>
+                        May 20th 2020
+                    </span>
+                    <a href="#">
+                        Read more
+                    </a>
+                </div>
+            </div>
+            <div class="no-limit-buzz--site-main-section-2-right" style="background-image: url('<?php echo THEME_ASSETS_URI; ?>/img/blog-img.png');">
+                <!-- silent is golden -->
+            </div>
+        </div>
+    </section>
+
 </main><!-- #main -->
 
 <?php
